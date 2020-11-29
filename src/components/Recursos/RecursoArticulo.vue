b3cfb5c445e7bd1cf9fdeb9fe409a68a35ef5d13
luispavel <luispavelvarela@gmail.com>
2020-11-29 16:17:51 -0600

    Consumiendo las APIs para hacer guardado articulo

--- src/components/Recursos/RecursoArticulo.vue
+++ src/components/Recursos/RecursoArticulo.vue
@@ -3,52 +3,65 @@
     <v-dialog v-model="dialog" persistent max-width="600px">
       <v-card>
         <v-card-title>
-          <span class="headline">User Profile</span>
+          <span class="headline">Nuevo Artículo</span>
         </v-card-title>
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field
-                  label="Legal middle name"
-                  hint="example of helper text only on focus"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Email*" required></v-text-field>
-              </v-flex>
+
+
               <v-flex xs12>
                 <v-text-field
-                  label="Password*"
-                  type="password"
-                  required
+                  v-model="titulo"
+                  label="Título del artículo*"
                 ></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6>
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
+
+              <v-flex xs12>
+                <v-text-field
+                  v-model="descripcionA"
+                  label="Descripción del artículo*"
+                ></v-text-field>
+                <span>El mensaje multilínea es:</span>
+                <p style="white-space: pre-line;">{{ descripcionA }}</p>
               </v-flex>
+              
+
               <v-flex xs12 sm6>
                 <v-autocomplete
+                  v-model="estado"
                   :items="estados"
                   item-text="nombre"
+                  return-object
                   label="Estados"
+                  @change="obtiene_municipios()"
                 ></v-autocomplete>
+                {{estado.id}}
               </v-flex>
+
+              <v-flex xs12>
+                <v-autocomplete
+                  v-model="municipio"
+                  :items="municipios"
+                  item-text="nombre"
+                  return-object
+                  label="Municipios"
+                  @change="obtiene_categorias()"
+                ></v-autocomplete>
+                {{municipio.id}}
+              </v-flex>
+
+              <v-flex xs12>
+                <v-autocomplete
+                  v-model="categoria"
+                  :items="categorias"
+                  item-text="descripcion"
+                  label="Categorias"
+                  return-object
+                ></v-autocomplete>
+                {{categoria.id}}
+              </v-flex>
+
             </v-layout>
           </v-container>
           <small>*indicates required field</small>
@@ -66,32 +79,57 @@
 </template>
 
 <script>
-
-import {getAPI} from '../../Api/axios-base';
+import { getAPI } from "../../Api/axios-base";
 export default {
   data: function () {
     return {
-      estados: [
-
-      ],
+      estados: [],
+      municipios:[],
       dialog: true,
+      articulo: {
+        nombre:null,
+        descripcion:null,
+        estado:null,
+        municipio:null,
+        usuario:1
+      },
+      estado:{
+        id:null
+      },
+      categorias:[],
+      categoria:{
+        id:null
+      },
+      municipio:{
+        id:null
+      }
+      
     };
   },
-  mounted:function(){
-    this.obtiene_estado()
+  mounted: function () {
+    this.obtiene_estado();
   },
 
   methods: {
-    obtiene_estado: function(){
+    obtiene_estado: function () {
       const self = this
-      getAPI.get("api/Estados").then(responde => {
-        console.log('Entre a obtiene estado')
-        console.log(responde.data)
-
-        self.estados = responde.data
-      })
+      getAPI.get("api/Estados").then((response) => {
+        self.estados = response.data
+      });
+    },
+    obtiene_municipios: function () {
+      const self = this
+      getAPI.get("api/Estados/"+ self.estado.id +"/GetMunicipio").then((res)=> {
+        self.municipios = res.data
+      });
+    },
+    obtiene_categorias: function() {
+      const self=this
+      getAPI.get("Recursos/api/Categorias").then((res)=> {
+        self.categorias = res.data
+      });
     }
-  }
+  },
 };
 </script>
 
