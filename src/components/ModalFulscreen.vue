96567b442608add0ad0f48748bfa13937b4c405d
Hector <hecjair1@gmail.com>
2020-12-27 23:21:03 -0600

    Agregando componente TarjetaRecurso

--- src/components/ModalFulscreen.vue
+++ src/components/ModalFulscreen.vue
@@ -59,110 +59,10 @@
         
       <v-container >
 
-
-        <template v-for="(value) in recursos">
-          <!-- <v-img
-            height="250"
-            src="https://www.quo.es/wp-content/uploads/2019/10/los-cinco-trabajos-que-por-ahora-no-podra-quitarte-un-robot.jpg"
-          ></v-img> -->
-          <Carousel> </Carousel>
-
-          <v-card-title>Nombre del recurso: {{value.nombreRecurso}}</v-card-title>
-
-          <v-card-text>
-            <v-row
-              align="center"
-              class="mx-0"
-            >
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ml-4">
-                4.5 (413)
-              </div>
-            </v-row>
-
-            <div class="my-4 subtitle-1">
-              $ • Italian, Cafe
-            </div>
-
-            <v-list-item-title class="headline mb-1">
-              ID: {{value.id}}
-            </v-list-item-title>
-            <v-list-item-title class="headline mb-1">
-              Usuario: {{value.Usuario}}
-            </v-list-item-title>
-            <v-list-item-title class="headline mb-1">
-              Descripcion: {{value.descripcion}}
-            </v-list-item-title>
-            <v-list-item-title class="headline mb-1">
-              Fecha de creacion: {{value.fechaCreacion}}
-            </v-list-item-title>
-            <v-list-item-title class="headline mb-1">
-              Fecha de modificación: {{value.fechaModificacion}}
-            </v-list-item-title>
-            <v-list-item-title class="headline mb-1">
-              Estado de procedencia: {{value.estado}}
-            </v-list-item-title>
-            <v-list-item-title class="headline mb-1">
-              Municipio de procedencia: {{value.municipio}}
-            </v-list-item-title>
-
-            <!-- <div>ID: {{value.id}}</div>
-            <div>Usuario: {{value.Usuario}}</div>
-            <div>Descripcion: {{value.descripcion}}</div>
-            <div>Fecha de creacion: {{value.fechaCreacion}}</div>
-            <div>Fecha de modificación: {{value.fechaModificacion}}</div>
-            <div>Estado de procedencia: {{value.estado}}</div>
-            <div>Municipio de procedencia: {{value.municipio}}</div> -->
-
-          </v-card-text>
-
-          <!-- <div> 
-            <v-divider class="mx-4"></v-divider>
-
-          </div> -->
-
-          <hr class="dashed">
-          
-          <br><br>
-          
-          
-
-          <!-- <v-card-title>Tonight's availability</v-card-title>
-
-          <v-card-text>
-            <v-chip-group
-              v-model="selection"
-              active-class="deep-purple accent-4 white--text"
-              column
-            >
-              <v-chip>5:30PM</v-chip>
-
-              <v-chip>7:30PM</v-chip>
-
-              <v-chip>8:00PM</v-chip>
-
-              <v-chip>9:00PM</v-chip>
-            </v-chip-group>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn
-              color="deep-purple lighten-2"
-              text
-              @click="reserve"
-            >
-              Reserve
-            </v-btn>
-          </v-card-actions> -->
-        </template>
+        <div v-if="dialog">
+          <TarjetaRecurso v-for="recurso in recursos" :recurso = recurso> </TarjetaRecurso>
+        </div>
+        
 
       </v-container>
       </v-card>
@@ -175,74 +75,86 @@
 
 import {getAPI} from '../Api/axios-base'
 import Carousel from '../components/Carousel'
+import TarjetaRecurso from './TarjetaRecurso'
 
-getAPI()
-  export default {
-    data () {
-      return {
-        dialog: false,
-        loading: false,
-        selection: 1,
-        headModalProp: String,
-        contentModalProp: String,
-        recursos: []
-      }
-    },
-    props: {
-      
-    },
-    components: {
-      Carousel
-    },
-    methods: {
-      showModal: function() {
+
+export default { 
+
+
+  mounted(){
+
+    
+  },
+
+
+  watch:{
+    headModalProp: function(){
+      this.getRecursos()
+    }
+  },
+  data () {
+    return {
+      dialog: false,
+      loading: false,
+      selection: 1,
+      headModalProp: String,
+      contentModalProp: String,
+      recursos: []
+    }
+  },
+  props: {
+    
+  },
+  components: {
+    Carousel,
+    TarjetaRecurso
+  },
+  methods: {
+    showModal: function() {
       this.dialog = true;
-      },
+    },
 
-      setTextModal: function (headModal, contentModal) {
-        // let myHeadModal = this.headModalProp
-        // let myContentModal = this.contentModalProp
+    setTextModal: function (headModal, contentModal) {
+      // let myHeadModal = this.headModalProp
+      // let myContentModal = this.contentModalProp
 
-        // let myHeadModal = headModal
-        // let myContentModal = contentModal
+      // let myHeadModal = headModal
+      // let myContentModal = contentModal
 
-        this.headModalProp =  headModal;
-        this.contentModalProp = contentModal;
-        
+      this.headModalProp =  headModal;
+      this.contentModalProp = contentModal;
 
-        getAPI.get("Recursos/api/Articulos/Estado/", {
-          params:{
-            "Estado": headModal
-          }
-          }
-        ).then(response => {
-            // console.log('AAA prro')
-            console.log(response.data)
-            this.recursos = response.data
-        })
-      },
-      reserve () {
-        this.loading = true
-
-        setTimeout(() => (this.loading = false), 2000)
-      },
-      // obtieneEstado: function() {
-      //   // const self = this
-      //   getAPI.get("api/Estados/").then(response => {
-      //     console.log('AAA prro')
-      //     console.log(response.data)
-      //     this.estados = response.data
-      //   })
-      // }
-
-      
-      
-
+      // return headModal;
       
 
       
     },
-  }
+    reserve () {
+      this.loading = true
+
+      setTimeout(() => (this.loading = false), 2000)
+    },
+
+
+     getRecursos: function() {
+            getAPI.get("Recursos/api/Articulos/Estado/", {
+                params:{
+                "Estado": this.headModalProp
+                }
+            }
+            ).then(response => {
+                // console.log('AAA prro')
+                console.log(response.data)
+                this.recursos = response.data
+            })
+        },
+    
+
+    
+
+    
+  },
+}
 </script>
 
 
---
+++ src/components/TarjetaRecurso.vue
@@ -0,0 +1,132 @@
+<template v-for="(value) in recursos">
+    <!-- <v-card-title>Nombre del recurso: {{value.nombreRecurso}}</v-card-title>4 -->
+    <div>
+        <!-- <div>{{recurso.id}}</div>
+        <div>{{recurso.nombreRecurso}}</div> -->
+
+        <!-- <v-img
+        height="250"
+        src="https://www.quo.es/wp-content/uploads/2019/10/los-cinco-trabajos-que-por-ahora-no-podra-quitarte-un-robot.jpg"
+        ></v-img> -->
+        <Carousel> </Carousel>
+
+        <v-card-title>Nombre del recurso: {{recurso.nombreRecurso}}</v-card-title>
+
+        <v-card-text>
+        <v-row
+            align="center"
+            class="mx-0"
+        >
+            <v-rating
+            :value="4.5"
+            color="amber"
+            dense
+            half-increments
+            readonly
+            size="14"
+            ></v-rating>
+
+            <div class="grey--text ml-4">
+            4.5 (413)
+            </div>
+        </v-row>
+
+        <div class="my-4 subtitle-1">
+            $ • Italian, Cafe
+        </div>
+
+        <v-list-item-title class="headline mb-1">
+            ID: {{recurso.id}}
+        </v-list-item-title>
+        <v-list-item-title class="headline mb-1">
+            Usuario: {{recurso.Usuario}}
+        </v-list-item-title>
+        <v-list-item-title class="headline mb-1">
+            Descripcion: {{recurso.descripcion}}
+        </v-list-item-title>
+        <v-list-item-title class="headline mb-1">
+            Fecha de creacion: {{recurso.fechaCreacion}}
+        </v-list-item-title>
+        <v-list-item-title class="headline mb-1">
+            Fecha de modificación: {{recurso.fechaModificacion}}
+        </v-list-item-title>
+        <v-list-item-title class="headline mb-1">
+            Estado de procedencia: {{recurso.estado}}
+        </v-list-item-title>
+        <v-list-item-title class="headline mb-1">
+            Municipio de procedencia: {{recurso.municipio}}
+        </v-list-item-title>
+
+        <!-- <div>ID: {{value.id}}</div>
+        <div>Usuario: {{value.Usuario}}</div>
+        <div>Descripcion: {{value.descripcion}}</div>
+        <div>Fecha de creacion: {{value.fechaCreacion}}</div>
+        <div>Fecha de modificación: {{value.fechaModificacion}}</div>
+        <div>Estado de procedencia: {{value.estado}}</div>
+        <div>Municipio de procedencia: {{value.municipio}}</div> -->
+
+        </v-card-text>
+
+        <!-- <div> 
+        <v-divider class="mx-4"></v-divider>
+
+        </div> -->
+
+        <hr class="dashed">
+        
+        <br><br>
+        
+        
+
+        <!-- <v-card-title>Tonight's availability</v-card-title>
+
+        <v-card-text>
+        <v-chip-group
+            v-model="selection"
+            active-class="deep-purple accent-4 white--text"
+            column
+        >
+            <v-chip>5:30PM</v-chip>
+
+            <v-chip>7:30PM</v-chip>
+
+            <v-chip>8:00PM</v-chip>
+
+            <v-chip>9:00PM</v-chip>
+        </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+        <v-btn
+            color="deep-purple lighten-2"
+            text
+            @click="reserve"
+        >
+            Reserve
+        </v-btn>
+        </v-card-actions> -->
+            
+            
+        </div>
+</template>
+
+<script>
+import { getAPI } from "../Api/axios-base";
+import Carousel from './Carousel'
+export default {
+    data() {
+        return {
+            recursos: []
+        }
+    },
+    components: {
+        Carousel
+    },
+    props: {
+        idEstado: String,
+        recurso:{},
+    },
+   
+
+}
+</script>
